c9ef2a92fbeb01b372a12ef748450534f23630de
Olivier <olikigotho@gmail.com>
2023-06-04 11:22:00 -0400

    Added functional logic in react and css

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,27 +1,19 @@
-import React, { Component } from "react";
-import {render } from "react-dom";
+import React, { useState } from "react";
+import { render } from "react-dom";
 
-/* This is the main javascript component, It is called by index.js 
-and it is to top of the react content rendered in the template: 
-index.html
-*/
-
-// Exporting the class as a default export to index.html
-export default class App extends Component {
-    constructor(props) {
-        super(props); // Calling the constructor of the parent class
-    }
-
-    // Render method to render the content within the template to the template
-    render() {
-        return (
-            <h1>Testing React Code</h1> // JSX code representing the content to be rendered
-        );
-    }
-}
+// This is the main React component.
+// It is called by index.js and represents the top-level content rendered in the template: index.html.
+const App = (props) => {
+const [content, setContent] = useState("Testing Now!")
+  return (
+    <h1>{ content } {props.name}</h1> // JSX code representing the content to be rendered
+  );
+};
 
 // Getting the element with the id "app" from the document
 const appDiv = document.getElementById("app");
 
 // Rendering the App component and attaching it to the appDiv element
-render(<App />, appDiv);
+render(<App name="Billy Bob"/>, appDiv);
+
+export default App;
