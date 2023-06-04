3576c430fae738920df7a2e06c761208e3c1bf6b
Olivier <olikigotho@gmail.com>
2023-06-04 12:32:53 -0400

    Added some comments and the exact path in Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,13 +4,14 @@ import Create from "./Create";
 import { BrowserRouter as Router, Route, Routes, Link, Redirect } from "react-router-dom";
 
 // Define a functional component named Home
+// remember to add paths to frontend/urls.py
 const Home = () => {
     const [content, setContent] = useState("This is the home page")
     return (
         <Router>
             <div>
                 <Routes>
-                    <Route path="/" element={ <p>{ content }</p>} />
+                    <Route exact path="/" element={ <p>{ content }</p>} />
                     <Route path="/join" element={ <JoinRoom/> } />
                     <Route path="/create" element={ <Create/> } />
                 </Routes>
